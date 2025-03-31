9bae8eb5072ef5fe8d0590e6507f22fb94876528
gausszhou <gausszhou@qq.com>
2025-04-01 00:55:35 +0800

    release: 0.0.1

--- src/zip.js
+++ src/zip.js
@@ -3,8 +3,9 @@ import zip from 'gulp-zip'
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 const manifest = require('../build/manifest.json')
+const packageJson = require('../package.json')
 
 gulp
   .src('build/**', { encoding: false })
-  .pipe(zip(`${manifest.name.replaceAll(' ', '-')}-${manifest.version}.zip`))
+  .pipe(zip(`${packageJson.name.replaceAll(' ', '-')}-${manifest.version}.zip`))
   .pipe(gulp.dest('package'))
